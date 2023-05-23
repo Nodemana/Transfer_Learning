d0a5788f63282d7cbbf12cda96e789a5b615f1ad
Nodemana <85973825+Nodemana@users.noreply.github.com>
2023-05-23 21:58:18 +1000

    Some of the barebones.

---
+++ MobileNetV2.py
@@ -0,0 +1,10 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import matplotlib.pyplot as plt
+import numpy as np      
+
+import tensorflow as tf 
+import tensorflow_addons as tfa
+from tensorflow import keras
+from keras import layers  
